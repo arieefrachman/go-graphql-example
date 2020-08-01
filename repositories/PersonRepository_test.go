058e0410d4a61365a596df7129c1d347c56e8e99
Arif Rakhman <kamucantiksekali@gmail.com>
2020-08-01 14:57:53 +0700

    Adding Find by ID test case

--- repositories/PersonRepository_test.go
+++ repositories/PersonRepository_test.go
@@ -3,32 +3,75 @@ package repositories
 import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/arieefrachman/go-graphql-example/infrastructures"
+	"github.com/arieefrachman/go-graphql-example/models"
 	"github.com/gocraft/dbr/v2"
 	"github.com/gocraft/dbr/v2/dialect"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestPersonRepository_GetAll(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	defer db.Close()
-
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection\n", err)
-	}
-
+func mockDB() (*PersonRepository, sqlmock.Sqlmock){
+	db, mock, _ := sqlmock.New()
 	personRepository := new(PersonRepository)
 	personRepository.DB = &infrastructures.SQLConnection{
 		Connection: &dbr.Connection{
 			DB:            db,
 			Dialect:       dialect.PostgreSQL,
-			EventReceiver: nil,
+			EventReceiver: &dbr.NullEventReceiver{},
 		},
 	}
+	return personRepository, mock
+}
 
-	rows := sqlmock.NewRows([]string{"id", "first_name", "last_name", "birth_date"}).
-		AddRow(1, "Arif", "Rakhman", "2010-01-01").
-		AddRow(2, "Dyta", "Vina", "2020-01-02")
+func TestPersonRepository(t *testing.T) {
+	t.Run("Get all data", func(t *testing.T) {
+		repo, mock := mockDB()
 
-	mock.ExpectQuery("^SELECT (.+) from persons$").WillReturnRows(rows)
+		rows := sqlmock.NewRows([]string{"id", "first_name", "last_name", "birth_date"}).
+			AddRow(1, "Arif", "Rakhman", "2010-01-01").
+			AddRow(2, "Dyta", "Vina", "2020-01-02")
 
+		mock.ExpectQuery("SELECT id, first_name, last_name, birth_date FROM persons").WillReturnRows(rows)
+
+		persons, err := repo.GetAll()
+
+		expectedResults := []models.Person{
+			{
+				ID:        1,
+				FirstName: "Arif",
+				LastName:  "Rakhman",
+				BirthDate: "2010-01-01",
+			},
+			{
+				ID:        2,
+				FirstName: "Dyta",
+				LastName:  "Vina",
+				BirthDate: "2020-01-02",
+			},
+		}
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedResults, persons)
+	})
+
+	t.Run("Find by ID", func(t *testing.T) {
+		repo, mock := mockDB()
+
+		ID := 1
+		row := sqlmock.NewRows([]string{"id", "first_name", "last_name", "birth_date"}).
+			AddRow(1, "Arif", "Rakhman", "2010-01-01")
+		mock.ExpectQuery("SELECT id, first_name, last_name, birth_date FROM persons WHERE*").WillReturnRows(row)
+
+		person, err := repo.FindByID(ID)
+
+		expectedResult := models.Person{
+			ID:        1,
+			FirstName: "Arif",
+			LastName:  "Rakhman",
+			BirthDate: "2010-01-01",
+		}
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedResult, person)
+	})
 }
